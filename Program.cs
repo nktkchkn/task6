8e9fddbacae166d546c9fd574d830ffa972e86ce
Nikita <nktkchkn21@yandex.ru>
2023-02-16 01:38:26 +0300

    Отредактировал задачу

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 //-3 -> нет
 //7 -> нет
 
-    Console.WriteLine("Введите число: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            if(num % 2 == 0) Console.WriteLine("Четное");
-            else Console.WriteLine("Нечетное");
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
